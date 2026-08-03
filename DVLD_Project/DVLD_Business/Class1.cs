f65a44337c6430b60386f9599ecf2aabe7cd400e
EsmaRuyaAd <pro20soma@gmail.com>
2026-08-03 19:14:57 +0100

    Add Project

---
+++ DVLD_Project/DVLD_Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DVLD_Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ DVLD_Project/DVLD_DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DVLD_DataAccess
+{
+    public class Class1
+    {
+    }
+}
